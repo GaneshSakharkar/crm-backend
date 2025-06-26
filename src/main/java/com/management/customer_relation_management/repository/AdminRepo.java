3260d5d30a26406b110ff3c74ef1cbe7f3e6c7ee
Ganesh Sakharkar <ganeshS@proteantech.in>
2025-06-26 18:56:59 +0530

    new super Admin add

--- src/main/java/com/management/customer_relation_management/repository/AdminRepo.java
+++ src/main/java/com/management/customer_relation_management/repository/AdminRepo.java
@@ -1,5 +1,6 @@
 package com.management.customer_relation_management.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import com.management.customer_relation_management.entities.Admin;
 public interface AdminRepo extends JpaRepository<Admin,Long> {
 
     Admin findByEmail(String email); 
+    
+    Optional<Admin> findByEmailAndName(String email,String name);
 }
--- src/main/java/com/management/customer_relation_management/role/Roles.java
+++ src/main/java/com/management/customer_relation_management/role/Roles.java
@@ -1,5 +1,5 @@
 package com.management.customer_relation_management.role;
 
 public enum Roles {
-    ADMIN,MANAGER
+    ADMIN,MANAGER,SUPER_ADMIN
 }
